3ad9666d60d27c51bc8fae1961f1b89dc678bfa6
Vitalii Kornyliuk <vitalii.kornyliuk@aspose.com>
2024-08-23 17:34:09 +0300

    update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testMatch: ["***/test/**/*-tests-*v*.ts"],
   testEnvironment: 'node',
-  bail: 1,
+  bail: true,
   verbose: true,
   setupFilesAfterEnv:  ['./jest.setup.js']
 };
